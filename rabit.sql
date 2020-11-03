25b8cae7784da12d18079ea55c26573c43eaeb90
csow <gajerberci@gmail.com>
2020-11-03 13:36:10 +0100

    Add files via upload

---
+++ rabit.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2020. Nov 03. 13:34
+-- Kiszolgáló verziója: 10.4.14-MariaDB
+-- PHP verzió: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `rabit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `advertisements`
+--
+
+CREATE TABLE `advertisements` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `advertisements`
+--
+
+INSERT INTO `advertisements` (`id`, `userid`, `title`) VALUES
+(1, 1, 'ADVERTISEMENT Shortcuts - The Easy Way'),
+(2, 2, 'How To Handle Every ADVERTISEMENT Challenge With Ease Using These Tips'),
+(3, 1, 'Revolutionize Your ADVERTISEMENT With These Easy-peasy Tips'),
+(4, 3, 'The ADVERTISEMENT Mystery Revealed'),
+(5, 4, 'Learn To (Do) ADVERTISEMENT Like A Professional'),
+(6, 4, 'How To Save Money with ADVERTISEMENT?'),
+(7, 3, 'I Don\'t Want To Spend This Much Time On ADVERTISEMENT. How About You?'),
+(8, 2, 'ADVERTISEMENT An Incredibly Easy Method That Works For All');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `name`) VALUES
+(1, 'Tommy'),
+(2, 'Adil'),
+(3, 'Noah'),
+(4, 'Bradley');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `advertisements`
+--
+ALTER TABLE `advertisements`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `advertisements`
+--
+ALTER TABLE `advertisements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `advertisements`
+--
+ALTER TABLE `advertisements`
+  ADD CONSTRAINT `advertisements_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
